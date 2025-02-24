6251d2873539609fde8d4c08310a5280aaba34ee
Nevan Naug <nevan@my.yorku.ca>
2025-02-24 15:32:04 -0500

    Redid the whole app
    The previous app was very buggy an hard to work with so I fixed up a lot of the code to make collaboration easier

--- README.md
+++
@@ -1,6 +0,0 @@
- HEAD
-# backend
-Backend code for my productivity app project
-
-# EECS-2311-Project-Repo
- f4aa6cc785ed3015933fd53e0956e8e93125dd32
---
+++ new-price-tracker/src/app/page.tsx
@@ -0,0 +1,102 @@
+//Home Page that User first sees when opening site
+
+import Link from "next/link"
+import { invoke } from "./blitz-server"
+import { LogoutButton } from "./(auth)/components/LogoutButton"
+import styles from "./styles/Home.module.css"
+import getCurrentUser from "./users/queries/getCurrentUser"
+
+export default async function Home() {
+  const currentUser = await invoke(getCurrentUser, null)
+  return (
+    <>
+      <div className={styles.globe} />
+      <div className={styles.container}>
+        <div className={styles.toastContainer}>
+          <p>
+            <strong>Welcome!</strong> Your app is ready, including user sign-up and log-in.
+          </p>
+        </div>
+
+        <main className={styles.main}>
+          <div className={styles.wrapper}>
+            <div className={styles.header}>
+              
+              <h1>The database & authentication is ready. Try it by signing up.</h1>
+
+              {/* Auth */}
+
+              <div className={styles.buttonContainer}>
+                {currentUser ? (
+                  <>
+                    <LogoutButton />
+                    <div>
+                      User id: <code>{currentUser.id}</code>
+                      <br />
+                      User role: <code>{currentUser.role}</code>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <Link href="/signup" className={styles.button}>
+                      <strong>Sign Up</strong>
+                    </Link>
+                    <Link href="/login" className={styles.loginButton}>
+                      <strong>Login</strong>
+                    </Link>
+                  </>
+                )}
+              </div>
+            </div>
+
+            <div className={styles.body}>
+              {/* Instructions */}
+              <div className={styles.instructions}>
+                <p>
+                  <strong>Created By: </strong>
+                </p>
+
+                <div>
+                  <div className={styles.code}>
+                    <span>1</span>
+                    <pre>
+                      <code>Nevan Naug</code>
+                    </pre>
+                  </div>
+
+                  <div className={styles.code}>
+                    <span>2</span>
+                    <pre>
+                      <code>Anna Maximova</code>
+                    </pre>
+                  </div>
+
+                  <div className={styles.code}>
+                    <span>3</span>
+                    <pre>
+                      <code>Ashdika Siddiqee</code>
+                    </pre>
+                  </div>
+
+                  <div className={styles.code}>
+                    <span>4</span>
+                    <pre>
+                      <code>Dilpreet Bansi</code>
+                    </pre>
+                  </div>
+
+                  <div className={styles.code}>
+                    <span>5</span>
+                    <pre>
+                      <code>Samiul Hossain</code>
+                    </pre>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  )
+}
