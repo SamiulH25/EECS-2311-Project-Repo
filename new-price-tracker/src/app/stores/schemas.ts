1363580e6e974d6e686ec4e8b3e1f0e3ceac8afd
naug-nevan-216527657 <nevan@my.yorku.ca>
2025-03-08 23:54:09 -0500

    store mutation

--- new-price-tracker/src/app/stores/schemas.ts
+++ new-price-tracker/src/app/stores/schemas.ts
@@ -1,14 +1,16 @@
-import { z } from "zod";
+import { z } from "zod"
 
 export const CreateStoreSchema = z.object({
   // template: __fieldName__: z.__zodType__(),
-});
+  name: z.string(),
+  location: z.string(),
+})
 export const UpdateStoreSchema = CreateStoreSchema.merge(
   z.object({
     id: z.number(),
   })
-);
+)
 
 export const DeleteStoreSchema = z.object({
   id: z.number(),
-});
+})
