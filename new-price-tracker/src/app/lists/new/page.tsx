daeddfbaeedae9eeaacc953a0d63d072ba95ec81
mx5-uni <mx5@my.yorku.ca>
2025-03-30 17:11:31 -0400

    Refined Items and Lists pages.

--- new-price-tracker/src/app/lists/new/page.tsx
+++ new-price-tracker/src/app/lists/new/page.tsx
@@ -1,19 +1,32 @@
 import { Metadata } from "next"
 import { Suspense } from "react"
 import { New__List } from "../components/NewList"
+import styles from "../../styles/Home.module.css"
 
 export const metadata: Metadata = {
-  title: "New Project",
-  description: "Create a new project",
+  title: "New List",
+  description: "Create a new list",
 }
 
 export default function Page() {
   return (
     <div>
-      <h1>Create New List</h1>
-      <Suspense fallback={<div>Loading...</div>}>
-        <New__List />
-      </Suspense>
+      <div className={styles.globe} />
+
+      <div className={styles.wrapper}>
+
+        <div className={styles.header}>
+          <h1><strong>Create</strong> New List</h1>
+        </div>
+
+        <div className={styles.centerList}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <New__List />
+          </Suspense>
+        </div>
+
+      </div>
+
     </div>
   )
 }
