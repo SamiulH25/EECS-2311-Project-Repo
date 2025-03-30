b6b0099b38aa04c6d183801fc431a5ecf2c77540
mx5-uni <mx5@my.yorku.ca>
2025-03-30 17:12:25 -0400

    Added GUI for All Stores page and New Store page.

--- new-price-tracker/src/app/stores/page.tsx
+++ new-price-tracker/src/app/stores/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import Link from "next/link";
 import { Suspense } from "react";
 import { StoresList } from "./components/StoresList";
+import styles from "../styles/Home.module.css"
 
 export const metadata: Metadata = {
   title: "Stores",
@@ -11,12 +12,28 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <div>
-      <p>
-        <Link href={"/stores/new"}>Create Store</Link>
-      </p>
-      <Suspense fallback={<div>Loading...</div>}>
-        <StoresList />
-      </Suspense>
+      <div className={styles.globe} />
+      
+      <div className={styles.wrapper}>
+
+        <div className={styles.header}>
+          <h1><strong>Available</strong> Stores</h1>
+        </div>
+
+        <div className={styles.centerList}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <StoresList />
+          </Suspense>
+
+          <p>
+            <Link href={"/stores/new"} className={styles.button}>
+            Create Store
+            </Link>
+          </p>
+        </div>
+
+      </div>
+
     </div>
   );
 }
