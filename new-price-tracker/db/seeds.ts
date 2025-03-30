55a0808976d182423835294f08a1e0fcb5c239cc
SamiulH25 <Sapasy2005@gmail.com>
2025-03-29 20:13:44 -0400

    Updated the database with the latest data obtained from the websites of each store. The data has been stored in DBData.json.

--- new-price-tracker/db/seeds.ts
+++ new-price-tracker/db/seeds.ts
@@ -3,7 +3,7 @@
 //Run 'blitz db seed' to run this program to seed the database
 
 import db from "./index"
-import stubDB from './stubDB.json'
+import stubDB from './DBData.json'
 
 const itemNames = ["Apple", "Banana", "Fish", "Beef", "Cat Food"];
 const itemPrices = [1.99, 0.99, 5.99, 10.99, 13.99];
@@ -32,6 +32,54 @@ const seed = async () => {
           },
           {
             name: stubDB.stores[i].items[4].name, price: stubDB.stores[i].items[4].price, upvotes: 0, downvotes: 0
+          },
+          {
+            name: stubDB.stores[i].items[5].name, price: stubDB.stores[i].items[1].price, upvotes: 0, downvotes: 0
+          },
+          {
+            name: stubDB.stores[i].items[6].name, price: stubDB.stores[i].items[2].price, upvotes: 0, downvotes: 0
+          },
+          {
+            name: stubDB.stores[i].items[7].name, price: stubDB.stores[i].items[3].price, upvotes: 0, downvotes: 0
+          },
+          {
+            name: stubDB.stores[i].items[8].name, price: stubDB.stores[i].items[4].price, upvotes: 0, downvotes: 0
+          },
+          {
+            name: stubDB.stores[i].items[9].name, price: stubDB.stores[i].items[1].price, upvotes: 0, downvotes: 0
+          },
+          {
+            name: stubDB.stores[i].items[10].name, price: stubDB.stores[i].items[2].price, upvotes: 0, downvotes: 0
+          },
+          {
+            name: stubDB.stores[i].items[11].name, price: stubDB.stores[i].items[3].price, upvotes: 0, downvotes: 0
+          },
+          {
+            name: stubDB.stores[i].items[12].name, price: stubDB.stores[i].items[4].price, upvotes: 0, downvotes: 0
+          },
+          {
+            name: stubDB.stores[i].items[13].name, price: stubDB.stores[i].items[1].price, upvotes: 0, downvotes: 0
+          },
+          {
+            name: stubDB.stores[i].items[14].name, price: stubDB.stores[i].items[2].price, upvotes: 0, downvotes: 0
+          },
+          {
+            name: stubDB.stores[i].items[15].name, price: stubDB.stores[i].items[3].price, upvotes: 0, downvotes: 0
+          },
+          {
+            name: stubDB.stores[i].items[16].name, price: stubDB.stores[i].items[4].price, upvotes: 0, downvotes: 0
+          },
+          {
+            name: stubDB.stores[i].items[17].name, price: stubDB.stores[i].items[1].price, upvotes: 0, downvotes: 0
+          },
+          {
+            name: stubDB.stores[i].items[18].name, price: stubDB.stores[i].items[2].price, upvotes: 0, downvotes: 0
+          },
+          {
+            name: stubDB.stores[i].items[19].name, price: stubDB.stores[i].items[3].price, upvotes: 0, downvotes: 0
+          },
+          {
+            name: stubDB.stores[i].items[20].name, price: stubDB.stores[i].items[4].price, upvotes: 0, downvotes: 0
           },
         ]
       }
