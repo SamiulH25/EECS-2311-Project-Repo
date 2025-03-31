ad0556cad829373f605cba4df55adbcb315b1f0f
mx5-uni <mx5@my.yorku.ca>
2025-03-30 23:56:58 -0400

    Added GUI to individual pages of Item and Store.

--- new-price-tracker/src/app/stores/components/Store.tsx
+++ new-price-tracker/src/app/stores/components/Store.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import deleteStore from "../mutations/deleteStore";
 import getStore from "../queries/getStore";
+import styles from "../../styles/Home.module.css"
 
 export const Store = ({ storeId }: { storeId: number }) => {
   const router = useRouter();
@@ -13,33 +14,43 @@ export const Store = ({ storeId }: { storeId: number }) => {
   return (
     <>
       <div>
-        <h1>{store.name}</h1>
+        <div className={styles.globe} />
+        <div className={styles.wrapper}>
+          <div className={styles.header}>
+            <h1>{store.name}</h1>
+            <h2>Items:</h2>
+          </div>
 
-        <h2>Items:</h2>
-        {store.items && store.items.length > 0 ? (
-          <ul>
-            {store.items.map((item, index) => (
-              <li key={index}>{item.name}</li>
-            ))}
-          </ul>
-        ) : (
-          <p>No items available in this store.</p>
-        )}
+          <div className={styles.centerBox}>
+            {store.items && store.items.length > 0 ? (
+              <ul>
+                {store.items.map((item, index) => (
+                  <li key={index}>{item.name}</li>
+                ))}
+              </ul>
+            ) : (
+              <p>No items available in this store.</p>
+            )}
+          </div>
 
-        <Link href={`/stores/${store.id}/edit`}>Edit</Link>
+          <div className={styles.buttonContainer}>
+            <Link href={`/stores/${store.id}/edit`}>Edit</Link>
 
-        <button
-          type="button"
-          onClick={async () => {
-            if (window.confirm("This will be deleted")) {
-              await deleteStoreMutation({ id: store.id });
-              router.push("/stores");
-            }
-          }}
-          style={{ marginLeft: "0.5rem" }}
-        >
-          Delete
-        </button>
+            <button
+              type="button"
+              onClick={async () => {
+                if (window.confirm("This will be deleted")) {
+                  await deleteStoreMutation({ id: store.id });
+                  router.push("/stores");
+                }
+              }}
+              style={{ marginLeft: "0.5rem" }}
+            >
+              Delete
+            </button>
+          </div>
+
+        </div>
       </div>
     </>
   );
