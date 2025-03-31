6f2abef9f91821ff46e6033717f448e2269939b3
naug-nevan-216527657 <nevan@my.yorku.ca>
2025-03-31 09:20:26 -0400

    schema update

--- new-price-tracker/src/app/items/schemas.ts
+++ new-price-tracker/src/app/items/schemas.ts
@@ -2,8 +2,14 @@ import { z } from "zod"
 
 export const CreateItemSchema = z.object({
   // template: __fieldName__: z.__zodType__(),
-  name: z.string().trim().pipe(z.string().min(2, { message: 'This field is required' })),
-  price: z.coerce.number().positive({message: "The price cannot be 0 or a negative number"}).lte(17),
+  name: z
+    .string()
+    .trim()
+    .pipe(z.string().min(2, { message: "This field is required" })),
+  price: z.coerce
+    .number()
+    .positive({ message: "The price cannot be 0 or a negative number" })
+    .lte(17),
   store: z.string(),
 })
 export const UpdateItemSchema = CreateItemSchema.merge(
