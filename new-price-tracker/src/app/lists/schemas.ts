d2199c3ad8c2c8446081f71e4a89ab5f7faeee01
naug-nevan-216527657 <nevan@my.yorku.ca>
2025-03-31 01:22:22 -0400

    Fixed bugs by updating validation

--- new-price-tracker/src/app/lists/schemas.ts
+++ new-price-tracker/src/app/lists/schemas.ts
@@ -2,7 +2,8 @@ import { z } from "zod"
 
 export const CreateListSchema = z.object({
   // template: __fieldName__: z.__zodType__(),
-  name: z.string(),
+  name: z.string().transform(value => value.replace(/\s+/g, ''))
+  .pipe(z.string().min(2, { message: 'This field is required' })),
   items: z.string().array().optional(),
 })
 export const UpdateListSchema = CreateListSchema.merge(
--- new-price-tracker/src/app/stores/schemas.ts
+++ new-price-tracker/src/app/stores/schemas.ts
@@ -2,7 +2,8 @@ import { z } from "zod"
 
 export const CreateStoreSchema = z.object({
   // template: __fieldName__: z.__zodType__(),
-  name: z.string(),
+  name: z.string().transform(value => value.replace(/\s+/g, ''))
+  .pipe(z.string().min(2, { message: 'This field is required' })),
 })
 export const UpdateStoreSchema = CreateStoreSchema.merge(
   z.object({
