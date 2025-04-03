f335d3c68314ab6046cf6cbb76d25ad4257684a8
naug-nevan-216527657 <nevan@my.yorku.ca>
2025-04-03 01:16:02 -0400

    loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,21 @@
+import styles from "./styles/Home.module.css"
+
 export default function Loading() {
   // Or a custom loading skeleton component
-  return "Loading..."
+  return (
+    <>
+    <div className={styles.globe} />
+      <div className={styles.container}>
+
+        <main className={styles.main}>
+          <div className={styles.wrapper}>
+            <div className={styles.header}>
+              <p>"Loading..."</p>
+            </div>
+          </div>
+        </main>
+      </div> 
+      </>  
+  )
+    
 }
